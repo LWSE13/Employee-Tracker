78f2fec59229bf75393ace9b59032fe173ec1aa4
LWSE13 <leewheeler888@gmail.com>
2024-04-16 00:08:15 +0100

    set up my Client class using the pg library in order to connect to my database

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const { Client } = require('pg');
+
+const client = new Client({
+  user: process.env.DB_USER,
+  host: 'localhost',
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
+});
+
+client.connect();
