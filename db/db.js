cabdbb8b2183380c50ef8e843ba2c4461d92666c
LWSE13 <leewheeler888@gmail.com>
2024-04-18 00:57:55 +0100

    finished work on all display methods and tidied up messy code

--- db/db.js
+++ db/db.js
@@ -33,21 +33,48 @@ class EmployeeDatabase extends Database {
 
     viewAllDepartments() {
         console.log('Viewing all departments');
-        return this.query('SELECT * FROM department').then((res) => {
+        return this.query(`
+            SELECT * 
+            FROM department
+        `).then((res) => {
             console.table(res.rows);
         });
     }
-
+    
     viewAllRoles() {
         console.log('Viewing all roles');
-        return this.query('SELECT * FROM role').then((res) => {
+        return this.query(`
+            SELECT 
+                role.id, 
+                role.title, 
+                role.salary, 
+                department.name AS department_name 
+
+            FROM department 
+
+            JOIN role ON role.department_id = department.id
+        `).then((res) => {
             console.table(res.rows);
         });
     }
-
+    
     viewAllEmployees() {
         console.log('Viewing all employees');
-        return this.query('SELECT * FROM employee').then((res) => {
+        return this.query(`
+            SELECT 
+                employee.id, 
+                CONCAT(employee.first_name, ' ', employee.last_name) AS full_name, 
+                role.title AS role_title, 
+                role.salary AS role_salary, 
+                department.name AS department_name, 
+                CONCAT(manager.first_name, ' ', manager.last_name) AS manager_name 
+
+            FROM employee 
+            
+            JOIN role ON employee.role_id = role.id 
+            JOIN department ON role.department_id = department.id 
+            LEFT JOIN employee AS manager ON employee.manager_id = manager.id
+        `).then((res) => {
             console.table(res.rows);
         });
     }
